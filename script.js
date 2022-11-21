0972fc6aab6bb756eaa4c6e84228d972f87e1a64
Haidar Khalid <110727504+HaidarKhalid@users.noreply.github.com>
2022-11-21 16:41:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let bO = document.querySelector(".bon")
+let bT = document.querySelector(".btw")
+let bt = document.querySelector(".bth")
+let bF = document.querySelector(".bfo")
+
+let testEL = document.querySelector(".test-el")
+
+
+function show() {
+    bO.classList.toggle("bone")
+    bT.classList.toggle("btwo")
+    bt.classList.toggle("bthree")
+    bF.classList.toggle("bfour")
+}
